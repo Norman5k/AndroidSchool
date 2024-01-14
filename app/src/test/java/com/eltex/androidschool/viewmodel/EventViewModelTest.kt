d167affe48484c1e5817565e416d4d9cf19a0eb3
Norman5k <mihailgaldin@gmail.com>
2024-01-14 15:40:54 +0700

    HomeWork #26

--- app/src/test/java/com/eltex/androidschool/viewmodel/EventViewModelTest.kt
+++ app/src/test/java/com/eltex/androidschool/viewmodel/EventViewModelTest.kt
@@ -2,6 +2,7 @@ package com.eltex.androidschool.viewmodel
 
 import com.eltex.androidschool.TestSchedulersFactory
 import com.eltex.androidschool.model.Event
+import com.eltex.androidschool.model.EventUiModel
 import com.eltex.androidschool.model.Status
 import com.eltex.androidschool.repository.EventRepository
 import io.reactivex.rxjava3.core.Completable
@@ -16,7 +17,7 @@ class EventViewModelTest {
         val testError = RuntimeException("Test error")
 
         val viewModel = EventViewModel(
-            repository =  object : EventRepository {
+            repository = object : EventRepository {
                 override fun getEvents(): Single<List<Event>> = Single.just(emptyList())
 
                 override fun participateById(id: Long): Single<Event> = Single.never()
@@ -32,11 +33,40 @@ class EventViewModelTest {
                 override fun deleteById(id: Long): Completable = Completable.error(testError)
 
             },
-            schedulersFactory =  TestSchedulersFactory
+            schedulersFactory = TestSchedulersFactory
         )
 
         viewModel.deleteById(1)
 
         assertEquals(testError, (viewModel.state.value.status as Status.Error).reason)
+    }
+
+    @Test
+    fun `deleteById success then success in state`() {
+        val testEvents: List<EventUiModel> = arrayListOf(EventUiModel(2))
+
+        val viewModel = EventViewModel(
+            repository = object : EventRepository {
+                override fun getEvents(): Single<List<Event>> = Single.just(arrayListOf(Event(1), Event(2)))
+
+                override fun participateById(id: Long): Single<Event> = Single.never()
+
+                override fun unparticipateById(id: Long): Single<Event> = Single.never()
+
+                override fun likeById(id: Long): Single<Event> = Single.never()
+
+                override fun dislikeById(id: Long): Single<Event> = Single.never()
+
+                override fun saveEvent(id: Long, content: String): Single<Event> = Single.never()
+
+                override fun deleteById(id: Long): Completable = Completable.complete()
+
+            },
+            schedulersFactory = TestSchedulersFactory
+        )
+
+        viewModel.deleteById(1)
+
+        assertEquals(testEvents, (viewModel.state.value.events))
     }
 }
